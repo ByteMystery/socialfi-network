9d4d238ca3c5c735bbd912c247897c94ddafa8d2
ByteMystery <info.aisearch@gmail.com>
2025-02-18 22:00:26 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "sql_socialfi";
+// MySQL Database Password
+$sql_db_pass = "d76617cb76366";
+// MySQL Database Name
+$sql_db_name = "sql_socialfi";
+
+// Site URL
+$site_url = "https://socialfi.com"; // e.g (http://example.com)
+
+$auto_redirect = true;
+
+// Purchase code
+$purchase_code = "56cfb629-cbc6-4105-bac2-11ca758b1eac"; // Your purchase code, don't give it to anyone.
+
+$siteEncryptKey = "ce0aa279c54050f9952015298079d56607c183df"; // Your site encrypt key, don't give it to anyone.
+?>
